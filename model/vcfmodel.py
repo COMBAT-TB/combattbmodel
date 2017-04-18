7b2101367c4fff31cbb5e10f2f8bb048caed17a5
Zipho Mashologu <zipho.m@gmail.com>
2017-04-18 09:54:33 +0200

    changes

--- model/vcfmodel.py
+++ model/vcfmodel.py
@@ -1,7 +1,6 @@
 # from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
 from core import *
 from galaxyuser import GalaxyUser
-from fasttree import FastTree
 
 
 # class Phenotype(GraphObject):
@@ -101,3 +100,16 @@ class Call(GraphObject):
         self.alt_allele = alt_allele
         self.gene = gene
         self.pk = pk
+
+class FastTree(GraphObject):
+    __primarykey__ = 'name'
+    name = Property()
+    data = Property()
+    history_id = Property()
+
+    from_variant_set = RelatedTo("VariantSet", "FROM_VARIANT_SET")
+
+    def __init__(self, name, data, history_id):
+        self.name = name
+        self.data = data
+        self.history_id = history_id
