86a2470d2742c3be8584b20be2525c61f1d16507
Zipho Mashologu <zipho.m@gmail.com>
2017-06-19 16:51:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='combat_tb_model',
-    version='0.0.2',
+    version='0.0.1',
     description='COMBAT-TB Graph Model,a Chado-derived graph model for genome annotation.',
     keywords='neo4j',
     packages=['combat_tb_model', 'combat_tb_model.model'],
