5a4e28d3c89f7b9d52313b0e26e5f51a5f5fba7f
thobalose <lose.thoba@gmail.com>
2018-07-31 11:33:23 +0200

    Update model

--- model/vcfmodel.py
+++ model/vcfmodel.py
@@ -1,7 +1,7 @@
 # from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
 import uuid
 
-from core import *
+from .core import *
 
 # class Phenotype(GraphObject):
 #     __primarykey__ = 'type'
@@ -16,45 +16,58 @@ REF_COL_ID = str(uuid.uuid3(uuid.NAMESPACE_DNS, 'www.internationalgenome.org'))
 
 
 class VariantSet(GraphObject):
+    """
+    VariantSet is the collection of CallSets
+    """
     __primarykey__ = 'col_id'
     name = Property()
     owner = Property()
     history_id = Property()
     col_id = Property()
 
-    has_variant = RelatedFrom("VariantSite", "BELONGS_TO_VSET")
-    has_call = RelatedTo("Call", "HAS_CALL")
-    forms_tree = RelatedFrom("FastTree", "FROM_VARIANT_SET")
-    has_callsets = RelatedFrom("CallSet", "HAS_CALLS_IN")
+    has_variant = RelatedTo("Variant", "BELONGS_TO")
+    forms_tree = RelatedTo("FastTree", "FROM_VARIANT_SET")
+    has_callsets = RelatedTo("CallSet", "CALL_SET")
 
     def __init__(self, name, owner, history_id=None, col_id=None):
         self.name = name
         self.owner = owner
         self.history_id = history_id
+        if not col_id:
+            col_id = REF_COL_ID
         self.col_id = col_id
 
 
 # CallSet = VCF file
 class CallSet(GraphObject):
+    """
+    CallSet is the VCF file
+    """
     __primarykey__ = 'name'
     name = Property()
     vset = Property()
     identifier = Property()
 
-    has_call = RelatedFrom("Call", "BELONGS_TO_CSET")
-    has_calls_in = RelatedTo("VariantSet", "HAS_CALLS_IN")
+    belongs_to_vset = RelatedFrom("VariantSet", "CALL_SET")
+    has_variants = RelatedFrom("Variant", "HAS_VARIANT")
 
     def __init__(self, name):
         self.name = name
         # self.vset = vset
 
 
+# VariantSite = Variant
 class Variant(GraphObject):
-    # NOTE: relies on FeatureLoc from core.py
+    """
+    VariantSite is the Variant
+    """
+    # NOTE: relies on Location from core.py
     # make __primarykey__ = VariantSet.name+POS
     __primarykey__ = 'pk'
 
+    pk = Property()
     pos = Property()
+    loc_in_seq = Property()
     feature_id = Property()
     biotype = Property()
     chrom = Property()
@@ -62,20 +75,23 @@ class Variant(GraphObject):
     alt_allele = Property()
     quality = Property()
     depth = Property()
+    consequence = Property()
     gene = Property()
     gene_id = Property()
     known = Property()
-    novel = Property()
-    pk = Property()
+    promoter = Property()
     impact = Property()
+    drug = Property()
 
     occurs_in = RelatedTo("Gene", "OCCURS_IN")
-    location = RelatedTo("FeatureLoc", "LOCATED_AT")
+    occurs_in_ = RelatedTo("RRna", "OCCURS_IN")
 
-    has_call = RelatedTo("Call", "HAS_CALL")
-    belongs_to_vset = RelatedTo("VariantSet", "BELONGS_TO_VSET")
+    location = RelatedTo("Location", "LOCATED_AT")
 
-    def __init__(self, chrom, pos, ref_allele, alt_allele, pk, impact, gene=None):
+    belongs_to_cset = RelatedTo("CallSet", "HAS_VARIANT")
+    resistant_to = RelatedTo("Drug", "RESISTANT_TO")
+
+    def __init__(self, chrom, pos, ref_allele, alt_allele, pk, impact=None, gene=None, consequence=None):
         self.chrom = chrom
         self.pos = pos
         self.ref_allele = ref_allele
@@ -83,9 +99,13 @@ class Variant(GraphObject):
         self.gene = gene
         self.pk = pk
         self.impact = impact
+        self.consequence = consequence
 
 
 class FastTree(GraphObject):
+    """
+    FastTree
+    """
     __primarykey__ = 'name'
     name = Property()
     data = Property()
